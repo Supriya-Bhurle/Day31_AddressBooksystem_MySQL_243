609ad6c10678a81f09ac13a505e27955d5c7da38
Supriya Bhurle <supriyabhurle28@gmail.com>
2023-02-20 19:30:51 +0530

    Added : AddressBook system MySQL Program

--- src/com/bridgelabz/addressbooksystem/AddressBookMySql.java
+++ src/com/bridgelabz/addressbooksystem/AddressBookMySql.java
@@ -1,2 +1,7 @@
-package com.bridgelabz.addressbooksystem;public class AddressBookMySql {
+package com.bridgelabz.addressbooksystem;
+
+public class AddressBookMySql {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Address Book System Program");
+    }
 }
